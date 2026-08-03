b905363d75b054a19edef09d1f737a055e92b927
Moimen Adel <moimenadel156@gmail.com>
2026-08-03 15:02:38 +0300

    update GoRouter configuration to include redirect logic for authenticated users

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 
 import 'package:practical_google_maps_example/core/routing/app_routes.dart';
+import 'package:practical_google_maps_example/core/utils/FirebaseHelper.dart';
 import 'package:practical_google_maps_example/features/AddOrder/AddOrderScreen.dart';
 import 'package:practical_google_maps_example/features/PlacePicker/view/MapPickerPage.dart';
 import 'package:practical_google_maps_example/features/auth/login_screen.dart';
@@ -9,37 +10,50 @@ import 'package:practical_google_maps_example/features/homeScreen/HomeScreen.dar
 import 'package:practical_google_maps_example/features/splash_screen/splash_screen.dart';
 
 class RouterGenerationConfig {
-  static GoRouter goRouter =
-      GoRouter(initialLocation: AppRoutes.splashScreen, routes: [
-    GoRoute(
-      name: AppRoutes.splashScreen,
-      path: AppRoutes.splashScreen,
-      builder: (context, state) => const SplashScreen(),
-    ),
-    GoRoute(
-      name: AppRoutes.loginScreen,
-      path: AppRoutes.loginScreen,
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      name: AppRoutes.registerScreen,
-      path: AppRoutes.registerScreen,
-      builder: (context, state) => const RegisterScreen(),
-    ),
-    GoRoute(
-      name: AppRoutes.mainScreen,
-      path: AppRoutes.mainScreen,
-      builder: (context, state) => const homeScreen(),
-    ),
-    GoRoute(
-      name: AppRoutes.addOrder,
-      path: AppRoutes.addOrder,
-      builder: (context, state) => const AddOrderScreen(),
-    ),
-    GoRoute(
-      name: AppRoutes.placePicked,
-      path: AppRoutes.placePicked,
-      builder: (context, state) => const MapPickerPage(),
-    )
-  ]);
+  static GoRouter goRouter = GoRouter(
+      redirect: (context, state) async {
+        final isLoggedIn = FirebaseService.currentUser != null;
+
+        final isGoingToHome = state.matchedLocation == AppRoutes.mainScreen;
+        final isGoingToLogin = state.matchedLocation == AppRoutes.loginScreen;
+
+        if (isLoggedIn && !isGoingToHome && isGoingToLogin) {
+          return AppRoutes.mainScreen;
+        }
+
+        return null;
+      },
+      initialLocation: AppRoutes.splashScreen,
+      routes: [
+        GoRoute(
+          name: AppRoutes.splashScreen,
+          path: AppRoutes.splashScreen,
+          builder: (context, state) => const SplashScreen(),
+        ),
+        GoRoute(
+          name: AppRoutes.loginScreen,
+          path: AppRoutes.loginScreen,
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          name: AppRoutes.registerScreen,
+          path: AppRoutes.registerScreen,
+          builder: (context, state) => const RegisterScreen(),
+        ),
+        GoRoute(
+          name: AppRoutes.mainScreen,
+          path: AppRoutes.mainScreen,
+          builder: (context, state) => const homeScreen(),
+        ),
+        GoRoute(
+          name: AppRoutes.addOrder,
+          path: AppRoutes.addOrder,
+          builder: (context, state) => const AddOrderScreen(),
+        ),
+        GoRoute(
+          name: AppRoutes.placePicked,
+          path: AppRoutes.placePicked,
+          builder: (context, state) => const MapPickerPage(),
+        )
+      ]);
 }
